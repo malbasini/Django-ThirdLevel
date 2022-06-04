605124d154a96626b66c2f12f5c1516055a4b11f
malbasini <malbasini@gmail.com>
2022-06-04 18:52:22 +0200

    Commit

--- DjangoThirdLevel/formsApp/forms.py
+++ DjangoThirdLevel/formsApp/forms.py
@@ -1,8 +1,24 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.core import validators
 
 class FormContatto(forms.Form):
-    nome = forms.CharField()
-    cognome = forms.CharField()
-    email = forms.EmailField()
-    contenuto = forms.CharField(widget=forms.Textarea
-    (attrs={'placeholder':'Area testuale!'}))
+    nome = forms.CharField(
+        widget=forms.TextInput(
+        attrs={"class": "form-control", "style":"width:500px;"}))
+    cognome = forms.CharField(
+        widget=forms.TextInput(
+        attrs={"class": "form-control" , "style":"width:500px;"}))
+    email = forms.EmailField(
+        widget=forms.TextInput( 
+        attrs={"class": "form-control" , "style":"width:500px;"}))
+    contenuto = forms.CharField(
+        widget=forms.Textarea(
+        attrs={"placeholder": "Area Testuale! Scrivi pure!", "class": "form-control","style":"width:500px;" }),
+        validators=[validators.MinLengthValidator(10)])
+
+    def clean_contenuto(self):
+        dati = self.cleaned_data["contenuto"]
+        if "parola" in dati:
+            raise ValidationError("Il contenuto inserito viole le norme del sito!")
+        return dati
